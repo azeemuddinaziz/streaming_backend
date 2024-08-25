19ccc60934fced6e8fba16e5b6190647050ee24a
Azeemuddin Aziz <azeemuddinaziz@Azeemuddins-MacBook-Air.local>
2024-08-25 18:59:27 +0530

    connect to mongoDB inside dedicated db folder: CONNECTION PATTERN 2

---
+++ src/db/connect.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./src" });
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+    );
+    console.log(
+      "Connection to MongoDB successful! HOST: ",
+      connectionInstance.connection.host
+    );
+  } catch (error) {
+    console.log("MongoDB connection FAILED: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { DB_NAME } from "./constants.js";
+import connectDB from "./db/connect.js";
 
 dotenv.config({ path: "./src" });
 
-(async () => {
-  try {
-    const connectInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
-    console.log(
-      "MongoDB connection successful, Host is: ",
-      connectInstance.connection.host
-    );
-  } catch (error) {
-    console.log("MongoDB connection FAILED: ", error);
-    throw error;
-  }
-})();
+connectDB();
+
+// (async () => {
+//   try {
+//     const connectInstance = await mongoose.connect(
+//       `${process.env.MONGODB_URI}/${DB_NAME}`
+//     );
+//     console.log(
+//       "MongoDB connection successful, Host is: ",
+//       connectInstance.connection.host
+//     );
+//   } catch (error) {
+//     console.log("MongoDB connection FAILED: ", error);
+//     throw error;
+//   }
+// })();
