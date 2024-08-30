e1c7895ac0345b248502753876946367d45fd77e
Azeemuddin Aziz <azeemuddinaziz@Azeemuddins-MacBook-Air.local>
2024-08-30 19:01:49 +0530

    add getUserChannelSubscriber and getSubscribedChannels controller and route associated with it.

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -53,4 +53,66 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   }
 });
 
-export { toggleSubscription };
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  try {
+    const { channelId } = req.params;
+
+    if (!channelId) throw new ApiError(400, "Channel Id is required.");
+
+    const channel = await User.findById(channelId);
+
+    if (!channel) throw new ApiError(400, "Channel not found.");
+
+    const subscribersList = await Subscription.find({
+      channel: channel,
+    }).select("-channel");
+
+    if (!subscribersList) throw new ApiError(400, "Subscribers not found.");
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          subscribersList,
+          "Subscribers list fetched successfully."
+        )
+      );
+  } catch (error) {
+    throw new ApiError(
+      500,
+      error?.message || error || "Failed to fetch subscribers."
+    );
+  }
+});
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  try {
+    const { subscriberId } = req.params;
+    if (!subscriberId) throw new ApiError(400, "Subscriber Id is required.");
+
+    const subscriber = await User.findById(subscriberId);
+    if (!subscriber) throw new ApiError(400, "Subscriber not found.");
+
+    const channelList = await Subscription.find({ subscriber }).select(
+      "-subscriber"
+    );
+    if (!channelList) throw new ApiError(500, "Could not fetch subscriptions.");
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, channelList, "Channel list fetched successfully")
+      );
+  } catch (error) {
+    throw new ApiError(
+      500,
+      error?.message ||
+        error ||
+        "Something went wrong while fetched channel list."
+    );
+  }
+});
+
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/subscribe/:channelId").post(verifyJWT, toggleSubscription);
+
+router.route("/subscribersList/:channelId").get(getUserChannelSubscribers);
+
+router.route("/channelList/:subscriberId").get(getSubscribedChannels);
 
 export default router;
