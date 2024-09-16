82fa522c212997fd6f045946d63b94de8698634e
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2024-09-16 20:30:23 +0530

    change origin to vercel hosted frontend url.

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://streaming-frontend-three.vercel.app/",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
